454054c320e15b2a6cd2ca6b3036d67eca86f3e5
johnsaveus <ioannis.savvas00@gmail.com>
2023-11-22 00:08:36 +0200

    Split_datasets

---
+++ train_test_split.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+''' For each class(band) there are 40 instances.
+    Those 40 instances are comprised of 8 albums -> 5 instances per album
+    From those 5 instances we will use 3 for training, 1 for validation and 1 for test
+    The 8th album of each band will always be used as a independent test dataset
+'''
+
+def main(path_to_data: str):
+
+    df = pd.read_csv(path_to_data)
+    train_csv = pd.DataFrame(columns=df.columns)
+    test_csv = pd.DataFrame(columns=df.columns)
+    
+    for i in range(0, len(df), 5):
+        train_data = df.iloc[i:i + 2]
+        test_data = df.iloc[i+2:i+5]
+
+        train_csv = pd.concat([train_csv,train_data],ignore_index=True)
+        test_csv = pd.concat([test_csv,test_data],ignore_index=True)
+
+    train_csv.to_csv(r'clean_data\train',index=True)
+    test_csv.to_csv(r'clean_data\test',index=True)
+
+    return None
+
+if __name__ == '__main__':
+    main('clean_data\csv_data')
