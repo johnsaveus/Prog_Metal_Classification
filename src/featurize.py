feaa968a567f384e0141fbd8e94ffb1283b029e9
johnsaveus <ioannis.savvas00@gmail.com>
2024-01-14 00:41:47 +0200

    pyaudio

--- src/featurize.py
+++ src/featurize.py
@@ -2,36 +2,57 @@ import pandas as pd
 import numpy as np
 import librosa
 from librosa import feature
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
-def feauturizer(wavs,labels):
+def featurizer(wavs):
 
     csv_matrix = []
     hp = 3000  
     for wav in wavs:   
-        y, _ = librosa.load(wav)
+        y, sr = librosa.load(wav)
         feature_vector = []
 
-        feature_vector.extend([feature.zero_crossing_rate(y,hop_length=hp),
-                               feature.rms(y=y,hop_length=hp),
-                               feature.spectral_centroid(y=y,hop_length=hp),
-                               feature.spectral_rolloff(y=y,hop_length=hp),
-                               feature.mfcc(y=y,hop_length=hp)])
-        
+        feature_vector.extend([feature.zero_crossing_rate(y=y),
+                               feature.rms(y=y),
+                               feature.spectral_centroid(y=y,sr=sr),
+                               feature.spectral_rolloff(y=y,sr=sr),
+                               feature.spectral_bandwidth(y=y,sr=sr),
+                               feature.spectral_contrast(y=y,sr=sr),
+                               feature.spectral_flatness(y=y),
+                               feature.mfcc(y=y,sr=sr),
+                               feature.chroma_stft(y=y,sr=sr),
+                               feature.chroma_cqt(y=y,sr=sr),
+                               feature.chroma_cens(y=y,sr=sr)])
+
         mean_features = [np.mean(feat) for feat in feature_vector]
+        #std_features = [np.std(feat) for feat in feature_vector]
         csv_matrix.append(mean_features)
 
     df_features = pd.DataFrame(csv_matrix, columns = ['zero_crossing_rate',
                                                       'rms',
                                                       'spectral_centroid',
                                                       'spectral_rolloff',
-                                                      'mfcc'])
-    df_features.insert(-1,'Band',label_encoding(labels=labels),True)
+                                                      'spectral_bandwidth',
+                                                      'spectral_contrast',
+                                                      'spectral_flatness',
+                                                      'mfcc',
+                                                      'chroma_stft',
+                                                      'chroma_cqt',
+                                                      'chroma_cens'])
         
     return df_features
     
 def label_encoding(labels):
     encoder = LabelEncoder()
-
     return encoder.fit_transform(labels)
-    
+
+'''def scaling(train,test):
+    scaler = StandardScaler()
+    scaler.fit(train)
+    scaled_train = pd.DataFrame(scaler.transform(train))
+    scaled_test = pd.DataFrame(scaler.transform(test))
+
+    return scaled_train, scaled_test'''
+
+def merge_labels(df,labels):
+    df.insert(11,'Band',label_encoding(labels=labels),True)
---
+++ src/pyaudio_features.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import librosa
+import pyAudioAnalysis.ShortTermFeatures as sF
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+
+def pyaudio_featurize(wavs):
+
+    feature_matrix = []
+    for wav in wavs:
+        feature_vector = []
+        y, sr = librosa.load(wav)
+        feats, names = sF.feature_extraction(y,sr, 0.05*sr, 0.025*sr)
+        feats_mean = np.mean(feats,axis=1)
+        feature_vector.extend(feats_mean)
+        feature_matrix.append(feature_vector)
+    df_features = pd.DataFrame(feature_matrix, columns = names)
+
+    return df_features
+
+def scaling(train,test):
+
+    names = train.columns
+    scaler = StandardScaler()
+    scaler.fit(train)
+    scaled_train = pd.DataFrame(scaler.transform(train),columns=names)
+    scaled_test = pd.DataFrame(scaler.transform(test),columns=names)
+
+    return scaled_train, scaled_test
+
+def label_encoding(labels):
+    encoder = LabelEncoder()
+    return encoder.fit_transform(labels)
+
+def merge_labels(df,labels):
+    df.insert(68,'Band',label_encoding(labels=labels),True)
